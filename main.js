ca6490ae436c09895bb28298ac6f4dc89f53de87
frzannaa <frzannaaffah@gmail.com>
2022-09-06 19:51:08 +0700

    Adding vertex and fragment shader codes

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function main() {
     var canvas = document.getElementById("MyCanvas");
+    var gl = canvas.getContext("webgl");
+
+    //Vertex Shader
+    var vertexShaderCode =
+        "void main () {" +
+        "}";
+
+    //Fragment Shader
+    var FragmentShaderCode = `
+    void main() {
+
+         }
+    
+    `;
 }
