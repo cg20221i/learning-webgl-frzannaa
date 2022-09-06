376d06a7fae1b818fde2f1414c994cdbf8b23b04
frzannaa <frzannaaffah@gmail.com>
2022-09-06 20:43:26 +0700

    add black background

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 function main() {
-    var canvas = document.getElementById("MyCanvas");
+    var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
     //Vertex Shader
-    var vertexShaderCode =
-        "void main () {" +
-        "}";
+    var vertexShaderCode = "void main () {" + "}";
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
 
     //Fragment Shader
-    var FragmentShaderCode = `
-    void main() {
-
-         }
-    
+    var fragmentShaderCode = `
+          void main () {
+              
+          }
     `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    //Comparing to C-Programming, we may imagine
+    // that up to this step we have created two object files (.o),
+    // For the vertex and fragment shaders
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // RGB and Alpha
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
 }
