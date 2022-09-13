2a5a1e176eaf3ba7b139a256be9b6d7fcd48ee1e
Farzana Afifah Razak <81352414+frzannaa@users.noreply.github.com>
2022-09-13 14:06:19 +0700

    draw one point

---
+++ main.js
@@ -0,0 +1,48 @@
+function main() {
+    var canvas = document.getElementById("myCanvas");
+    var gl = canvas.getContext("webgl");
+
+    // VERTEX SHADER
+    var vertexShaderCode = `
+        void main () {
+            gl_PointSize = 10.0;
+            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+            // gl_Position is the final destination for storing
+            //  positional data for the rendered vertex
+        }
+    `;
+
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+
+    // FRAGMENT SHADER
+    var fragmentShaderCode = `
+        void main() {
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+            // Blue = R:0, G:0, B:1, A:1
+            // gl_FragColor is the final destination for storing
+            //  color data for the rendered fragment
+        }
+    `;
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+
+    // Comparing to C programming, we my imagine
+    // that up to this step we have created two
+    // object files (.o) for the vertex and fragment shader
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+
+    gl.clearColor(1.0, 0.75, 0.79, 1.0);
+    //R,G,B,Alpha
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINT, 0, 1);
+}
