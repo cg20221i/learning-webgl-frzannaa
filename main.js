d7616c6262993d84461975408096bb893c888aca
frzannaa <frzannaaffah@gmail.com>
2022-09-20 14:38:24 +0700

    colorful rectangle

--- main.js
+++ main.js
@@ -2,42 +2,51 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    // A(0.5,0.5)
-    // B(0.0, 0.0)
-    // C(-0.5, 0.5)
+    /**
+     * A (  0.5,  0.5)  Red     (1.0, 0.0, 0.0)
+     * B (  0.0,  0.0)  Green   (0.0, 1.0, 0.0)
+     * C ( -0.5,  0.5)  Blue    (0.0, 0.0, 1.0)
+     * D (  0.0,  1.0)  Black   (0.0, 0.0, 0.0)
+     */
 
     var vertices = [
-        0.5, 0.5,
-        0.0, 0.0, -0.5, 0.5,
-        0.0, 1.0,
+        0.5, 0.5, 1.0, 0.0, 0.0,
+        0.0, 0.0, 0.0, 1.0, 0.0, -0.5, 0.5, 0.0, 0.0, 1.0,
+        0.0, 1.0, 0.0, 0.0, 0.0
     ];
 
-    //create a linked list for storing the vertices data in the GPU realm
+    // Create a linked-list for storing the vertices data in the GPU realm
     var buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
     // VERTEX SHADER
     var vertexShaderCode = `
-    attribute vec2 aPosition;
-    uniform float uTheta;
+        attribute vec2 aPosition;
+        attribute vec3 aColor;
+        uniform float uTheta;
+        varying vec3 vColor;
         void main () {
-            gl_PointSize = 15.0;
+            //gl_PointSize = 15.0;
             vec2 position = vec2(aPosition);
             position.x = -sin(uTheta) * aPosition.x + cos(uTheta) * aPosition.y;
             position.y = sin(uTheta) * aPosition.y + cos(uTheta) * aPosition.x;
             gl_Position = vec4(position, 0.0, 1.0);
-    }
+            // gl_Position is the final destination for storing
+            //  positional data for the rendered vertex
+            vColor = aColor;
+        }
     `;
-
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
     // FRAGMENT SHADER
     var fragmentShaderCode = `
+        precision mediump float;
+        varying vec3 vColor;
         void main() {
-            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+            gl_FragColor = vec4(vColor, 1.0);
             // Blue = R:0, G:0, B:1, A:1
             // gl_FragColor is the final destination for storing
             //  color data for the rendered fragment
@@ -47,9 +56,9 @@ function main() {
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
 
-    // Comparing to C programming, we my imagine
-    // that up to this step we have created two
-    // object files (.o) for the vertex and fragment shader
+    // Comparing to C-Programming, we may imagine
+    //  that up to this step we have created two
+    //  object files (.o), for the vertex and fragment shaders
 
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
@@ -57,30 +66,42 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    // local var
+    // Local variables
     var theta = 0.0;
 
-    // ALL the qualifiers need by shadeers
-    var uTheta = gl.getUniformLocation(shaderProgram, 'uTheta');
+    // All the qualifiers needed by shaders
+    var uTheta = gl.getUniformLocation(shaderProgram, "uTheta");
 
-    //teach the GPU how to collect
-    //the positional values from ARRAY_BUFFER
-    //for each vertex being processed
-
+    // Teach the GPU how to collect
+    //  the positional values from ARRAY_BUFFER
+    //  for each vertex being processed
     var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0);
     gl.enableVertexAttribArray(aPosition);
+    var aColor = gl.getAttribLocation(shaderProgram, "aColor");
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT);
+    gl.enableVertexAttribArray(aColor);
 
     function render() {
         gl.clearColor(1.0, 0.75, 0.79, 1.0);
+        //Red, Green, Blue, Alpha
         gl.clear(gl.COLOR_BUFFER_BIT);
         theta += 0.01;
         gl.uniform1f(uTheta, theta);
         gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
         requestAnimationFrame(render);
-
     }
     requestAnimationFrame(render);
-
-
 }
